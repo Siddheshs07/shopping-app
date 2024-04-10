0a57d75e57bd119916aeabe84a231fa1660e1d37
Siddheshs07 <siddeshsalimath07@gmail.com>
2024-04-10 22:26:06 +0530

    ui almost complete-1

--- src/utils/db-util.ts
+++ src/utils/db-util.ts
@@ -1,8 +1,8 @@
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 
-export async function connectDataBase() {
-  const URL =
-    "mongodb+srv://siddeshsalimath3 :3MgPb0J1gvkXXt7C@cluster0.e1zs6p4.mongodb.net/?retryWrites=true&w=majority";
-  const client = await MongoClient.connect(URL);
-  return client;
-}
+// export async function connectDataBase() {
+//   const URL =
+//     "mongodb+srv://siddeshsalimath3 :3MgPb0J1gvkXXt7C@cluster0.e1zs6p4.mongodb.net/?retryWrites=true&w=majority";
+//   const client = await MongoClient.connect(URL);
+//   return client;
+// }
